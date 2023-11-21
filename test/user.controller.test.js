c108cf53f13b9f40bec00bca45cf03b1d6b1eb3b
Léo Ségalini <130433746+Lejardinierdu33@users.noreply.github.com>
2023-11-21 15:33:22 +0400

    commit

--- test/user.controller.test.js
+++ test/user.controller.test.js
@@ -1,3 +1,6 @@
+//During the test the env variable is set to test process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = 'test';
+
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { server } = require('../backend/server');
